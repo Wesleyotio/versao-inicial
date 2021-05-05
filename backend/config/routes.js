72d85766f70fcc6648b52c268505a4b75fa0f478
José Wesley <wesleymagnos@gmail.com>
2021-05-05 10:08:53 -0300

    API de autenticação

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,6 +1,11 @@
 
 
 module.exports = app => {
+
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
         .get(app.api.user.get)
         .post(app.api.user.save)
