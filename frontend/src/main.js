78f0614d24e1803ebf92bb165251bd512a42f3fb
José Wesley <wesleymagnos@gmail.com>
2023-08-16 17:28:06 -0300

    conection simulation from backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,6 +9,9 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
+//GAMBIARRA 
+require('axios').defaults.headers.common['Authorization'] = ' bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkrDo28gdGVzdGUiLCJlbWFpbCI6Imphb3Rlc3RlQGVtYWlsLmNvbSIsImFkbWluIjpmYWxzZSwiaWF0IjoxNjkyMTUyOTE3LCJleHAiOjE2OTI0MTIxMTd9.vJA1XRO5-Du6YxxmvgClnnkREcwto-Qgd_ACZ8z2AEE'
+
 new Vue({
   store,
   router,
