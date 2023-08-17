8065cdcfac4591d23a3228f9b91d1c7b0d576851
José Wesley <wesleymagnos@gmail.com>
2023-08-17 20:27:09 -0300

    add tabs admin

--- frontend/src/components/admin/AdminPages.vue
+++ frontend/src/components/admin/AdminPages.vue
@@ -2,16 +2,35 @@
     <div class="admin-pages">
         <Pagetitle icon="fa fa-cogs" main="Administração do Sistema"
                 sub="Cadastro & Cia" />
+        <div class="admin-pages-tabs">
+            <b-card no-body>
+                <b-tabs card>
+                    <b-tab title="Artigos" active>
+                        <ArticleAdmin/>
+                    </b-tab>
+                    <b-tab title="Categorias" active>
+                        <CategoryAdmin/>
+                    </b-tab>
+                    <b-tab title="Usuários" active>
+                        <UserAdmin/>
+                    </b-tab>
+
+                </b-tabs>
+            </b-card>
+        </div>
     </div>
 </template>
 
 <script>
 
 import Pagetitle from '../template/Pagetitle.vue';
+import ArticleAdmin from './ArticleAdmin.vue';
+import CategoryAdmin from './CategoryAdmin.vue';
+import UserAdmin from './UserAdmin.vue';
 
 export default {
     name: 'AdminPages',
-    components: { Pagetitle }
+    components: { Pagetitle, ArticleAdmin, CategoryAdmin, UserAdmin }
 }
 </script>
 
