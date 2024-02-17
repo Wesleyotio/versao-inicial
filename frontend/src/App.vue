a42fb202df13d61218069df19184f23d76f406df
José Wesley <wesleymagnos@gmail.com>
2024-02-17 00:07:23 -0300

    updated logout and screen loading

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,24 +5,62 @@
 			:hideUserDropDown="!user" 
 		/>
 		<Menu v-if="user" />
-		<Content/>
+		<Loading v-if="validatingToken"/>
+		<Content v-else/>
 		<Footer/>
 	</div>
 </template>
 
 <script>
+import axios from "axios"
+import { baseAPIUrl, userKey } from "@/global"
 import { mapState } from 'vuex';
 import Header from './components/template/Header.vue';
 import Menu from './components/template/Menu.vue';
 import Footer from './components/template/Footer.vue';
 import Content from './components/template/Content.vue';
+import Loading from './components/template/Loading.vue';
 
 
 
 export default {
 	name: "App",
-	components: { Header, Menu, Content, Footer},
-	computed: mapState(['isMenuVisible', 'user'])
+	components: { Header, Menu, Content, Footer, Loading },
+	computed: mapState(['isMenuVisible', 'user']),
+	data: function() {
+		return {
+			validatingToken: true
+		}
+	},
+	methods: {
+		async validateToken() {
+			this.validatingToken = true
+
+			const json = localStorage.getItem(userKey)
+			const userData = JSON.parse(json)
+			this.$store.commit('setUser', null)
+
+			if (!userData) {
+				this.validatingToken = false
+				this.$router.push({ name: 'auth' })
+				return
+			}
+
+			const res = await axios.post(`${baseAPIUrl}/validateToken`, userData)
+
+			if (res.data) {
+				this.$store.commit('setUser', userData)
+			} else {
+				localStorage.removeItem(userKey)
+				this.$router.push({ name: 'auth' })
+			}
+
+			this.validatingToken = false
+		}
+	},
+	created() {
+		this.validateToken()
+	}
 }
 </script>
 
